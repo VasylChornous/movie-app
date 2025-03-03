374bc3b7b079fa9bf10791b960847b028b9f5caf
Vasyl <vasya.chornous@gmail.com>
2025-03-04 01:16:24 +0200

    add pages and layout

---
+++ src/pages/movies/Movies.jsx
@@ -0,0 +1,17 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+const Movies = () => {
+  return (
+    <Container maxW={"container.xl"}>
+      <Heading
+        as="h2"
+        fontSize={"md"}
+        textTransform={"uppercase"}
+      >
+        Discover Movies
+      </Heading>
+    </Container>
+  );
+};
+
+export default Movies;
